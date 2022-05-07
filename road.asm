fb6b765ea8bb09442a1a84bdcd7b8bd94ecf0792
raffy <rafpgn@gmail.com>
2022-05-07 21:34:21 +0200

    Linee carreggiata

--- road.asm
+++ road.asm
@@ -62,14 +62,14 @@ roadSpeedMatchYes:
 
 	Draw: {
 		// *** If Pari o Dispari Then Goto *** //
-		jmp !rowEven+
-		// lda RoadPariOrDispari 
-		// cmp #ROAD_PARI
-		// beq !rowEven+
+		// jmp !rowOdd+
+		lda RoadPariOrDispari 
+		cmp #ROAD_PARI
+		beq !rowEven+
 		
-		// lda RoadPariOrDispari		
-		// cmp #ROAD_DISPARI
-		// beq !rowOdd+		
+		lda RoadPariOrDispari		
+		cmp #ROAD_DISPARI
+		beq !rowOdd+		
 		rts
 
 	!rowEven:		
@@ -77,8 +77,20 @@ roadSpeedMatchYes:
 		// Pari: Pieno
 		lda #$04
 		//sta SCREEN_RAM + 0 * SCREEN_ROW_LENGTH + $13 //1
-		sta $043b
-		sta $048b
+		sta $043b //1
+		sta $048b //2 +$50
+		sta $04db //3
+		sta $052b //4
+		sta $057b //5
+		sta $05cb //6
+		sta $061b //7
+		sta $066b //8
+		sta $06bb //9
+		sta $070b //10
+		sta $075b //11
+		sta $07ab //12
+		
+		
 		//sta SCREEN_RAM + 2 * SCREEN_ROW_LENGTH  //2
 		// sta SCREEN_RAM + 4 * SCREEN_ROW_LENGTH + $13 //3
 		// sta SCREEN_RAM + 6 * SCREEN_ROW_LENGTH + $13 //4
@@ -92,8 +104,20 @@ roadSpeedMatchYes:
 		// sta SCREEN_RAM + 22 * SCREEN_ROW_LENGTH + $13 //12
 		// sta SCREEN_RAM + 24 * SCREEN_ROW_LENGTH + $13 //13
 		// Dispari: Vuoto
-		lda #$00
-		sta $0463
+		lda #$00 //$00
+		sta $0463 //1
+		sta $0463+$50 //2
+		sta $0463+$50+$50 //3
+		sta $0463+$50+$50+$50 //4
+		sta $0463+$50+$50+$50+$50 //5
+		sta $0463+$50+$50+$50+$50+$50 //6
+		sta $0463+$50+$50+$50+$50+$50+$50 //7
+		sta $0463+$50+$50+$50+$50+$50+$50+$50 //8
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50 //9
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50 //10
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50 //11
+		// sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50 //12
+
 		// sta SCREEN_RAM + 1 * SCREEN_ROW_LENGTH + $13 //1
 		// sta SCREEN_RAM + 3 * SCREEN_ROW_LENGTH + $13 //2
 		// sta SCREEN_RAM + 5 * SCREEN_ROW_LENGTH + $13 //3
@@ -112,6 +136,20 @@ roadSpeedMatchYes:
 		// .break
 		// Pari: Niente
 		lda #$00
+			
+		sta $043b //1
+		sta $048b //2 +$50
+		sta $04db //3
+		sta $052b //4
+		sta $057b //5
+		sta $05cb //6
+		sta $061b //7
+		sta $066b //8
+		sta $06bb //9
+		sta $070b //10
+		sta $075b //11
+		sta $07ab //12
+		// sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50 //12
 		//sta SCREEN_RAM + 0 * SCREEN_ROW_LENGTH + $13 //1
 		// sta SCREEN_RAM + 2 * SCREEN_ROW_LENGTH + $13 //2
 		// sta SCREEN_RAM + 4 * SCREEN_ROW_LENGTH + $13 //3
@@ -127,6 +165,18 @@ roadSpeedMatchYes:
 		//sta SCREEN_RAM + 24 * SCREEN_ROW_LENGTH + $13 //13 
 		// Dispari: Pieno
 		lda #$04
+		sta $0463 //1
+		sta $0463+$50 //2
+		sta $0463+$50+$50 //3
+		sta $0463+$50+$50+$50 //4
+		sta $0463+$50+$50+$50+$50 //5
+		sta $0463+$50+$50+$50+$50+$50 //6
+		sta $0463+$50+$50+$50+$50+$50+$50 //7
+		sta $0463+$50+$50+$50+$50+$50+$50+$50 //8
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50 //9
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50 //10
+		sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50 //11
+		// sta $0463+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50+$50 //12
 		// sta SCREEN_RAM + 1 * SCREEN_ROW_LENGTH + $13 //1
 		// sta SCREEN_RAM + 3 * SCREEN_ROW_LENGTH + $13 //2
 		// sta SCREEN_RAM + 5 * SCREEN_ROW_LENGTH + $13 //3
